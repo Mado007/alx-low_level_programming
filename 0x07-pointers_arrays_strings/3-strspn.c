e5e72bac85e3f7acde44798dc2239287962ee976
Mado007 <mahmodeid2019@gmail.com>
2023-04-03 08:52:08 -0700

    hot fix for betty style

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,11 +2,9 @@
 
 /**
 * _strspn - Gets the length of a prefix substring.
-* @s: String where substring will look
-*
-* @accept: Substring of accepted chars
-*
-* Returns the number of bytes in consist only of bytes from accept 
+* @s: String where substring will look.
+* @accept: Substring of accepted chars.
+* Return: Length of occurrence.
 */
 
 unsigned int _strspn(char *s, char *accept)
