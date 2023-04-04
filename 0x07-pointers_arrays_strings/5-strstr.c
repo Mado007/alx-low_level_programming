336a0f3c1fcb9fe48d376939ad84e539b8a293ee
Mado007 <mahmodeid2019@gmail.com>
2023-04-04 06:26:57 -0700

    Hot FIX our betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
-* _strstr - function locate
-* @haystack: pointer to char
-* @needle: pointer to char
-*
-* Return: pointer to the beginning of the located substring, or NULL if the substring is not found
-*/
+ * _strstr - a function that locates a sub-string
+ * @haystack: locate a sub-string
+ * @needle: sub-string to locate
+ *
+ * Return: pointer to the beginning of the located sub-string,
+ * or NULL, if sub-string is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
-char *h = haystack;
-char *n = needle;
+	char *h = haystack;
+	char *n = needle;
 
-while (*h)
-{
-n = needle;
-h = haystack;
-while (*n)
-{
-if (*h == *n)
-{
-n++;
-h++;
-}
-else
-break;
-}
-if (*n == '\0')
-return (haystack);
-haystack++;
-}
-return (0);
+	while (*h)
+	{
+		n = needle;
+		h = haystack;
+		while (*n)
+		{
+			if (*h == *n)
+			{
+				n++;
+				h++;
+			}
+			else
+				break;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
 }
