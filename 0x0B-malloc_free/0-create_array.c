4bbf250a4fd5e0a4200997bd245e4405dba02179
Mado007 <mahmodeid2019@gmail.com>
2023-04-11 09:09:04 -0700

    hot fix:Add Function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,22 +5,28 @@
  * with a specific char
  * @size: size of the array
  * @c: char to be initialized with
+ * Return: pointer
  **/
 char *create_array(unsigned int size, char c)
 {
-char *stri;
-unsigned int i;
+char *str;
+unsigned int i = 0;
 
 if (size == 0)
 return (NULL);
-stri = malloc(sizeof(char) * size);
-if (stri == NULL)
+
+str = (char *)malloc(size * sizeof(char));
+
+if (str == 0)
+{
 return (NULL);
-i = 0;
+}
+
+else
 while (i < size)
 {
-stri[i] = c;
+str[i] = c;
 i++;
 }
-return (stri);
+return (str);
 }
