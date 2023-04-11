b30a1c5a6c88936263049ebb4fc18bcf9d587803
Mado007 <mahmodeid2019@gmail.com>
2023-04-11 09:14:21 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy
+ * @str: pointer to string
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+char *strDup;
+int i, j;
+
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+i++;
+i++;
+strDup = malloc(sizeof(*str) * i);
+if (strDup == NULL)
+return (NULL);
+j = 0;
+while (str[j] != '\0')
+
+{
+strDup[j] = str[j];
+j++;
+}
+return (strDup);
+}
