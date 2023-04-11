96cf82664a9729c92db3cde29a14cd7a9335d1e4
Mado007 <mahmodeid2019@gmail.com>
2023-04-11 10:02:31 -0700

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *argstostr - concatenates all arguements to the program
+ * @ac: arguement count
+ * @av: pointer to arguements
+ * Return: pointer to new space in memory or null
+ **/
+char *argstostr(int ac, char **av)
+{
+char *str;
+int count = 0, a = 0, b = 0, c = 0;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+while (a < ac)
+{
+b = 0;
+while (av[a][b] != '\0')
+{
+count++;
+b++;
+}
+a++;
+}
+count = count + ac + 1;
+str = malloc(sizeof(char) * count);
+if (str == NULL)
+{
+return (NULL);
+}
+for (a = 0; a < ac; a++)
+{
+for (b = 0; av[a][b] != '\0'; b++)
+{
+str[c] = av[a][b];
+c++;
+}
+str[c] = '\n';
+c++;
+}
+return (str);
+}
