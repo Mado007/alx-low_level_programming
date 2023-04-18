88fbe8e32fba3dacc20949df51010b6d772e5c16
Mado007 <mahmodeid2019@gmail.com>
2023-04-17 19:52:42 -0700

    Task2- File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program check the code for Holberton School students.
+ *
+ * Return: Nothing
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
