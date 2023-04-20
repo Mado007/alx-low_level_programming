36be9724e7968ce77b82c7c3084ee2ba9fed27bf
Mado007 <mahmodeid2019@gmail.com>
2023-04-20 16:50:51 -0700

    Our Header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCT_HEADER_
+#define _VARIADIC_FUNCT_HEADER_
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
