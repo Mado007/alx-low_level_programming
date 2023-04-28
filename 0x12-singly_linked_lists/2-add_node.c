eb0e988f4370a07123d5602e15b794df40fb1123
Mado007 <mahmodeid2019@gmail.com>
2023-04-27 18:02:24 -0700

    fix func

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,7 +21,7 @@ return (d);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-int d, len;
+int i, len;
 char *content;
 list_t *new;
 
@@ -32,8 +32,8 @@ new = *head;
 content = malloc((len + 1) * sizeof(char));
 if (content == NULL)
 return (NULL);
-for (d = 0; str[d]; d++)
-content[d] = str[d];
+for (i = 0; str[i]; i++)
+content[i] = str[i];
 new = malloc(sizeof(list_t));
 if (new == NULL)
 {
