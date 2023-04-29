bc9393fd3fc987ee971e6e49088127daf7e75ae7
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2023-04-29 19:24:59 +0200

    Update function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t counter = 0;
+	size_t counter = 0;
 
-if (!h)
-return (0);
-while (h)
-{
-printf("%d\n", h->n);
-h = h->next;
-counter++;
-}
-return (counter);
+	if (!h)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
 }
