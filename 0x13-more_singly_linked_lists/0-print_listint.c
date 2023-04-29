e1ee9e2863cfcd4f94a2ac30f867fc2d28ef51d8
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2023-04-29 19:30:10 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,20 +4,20 @@
 /**
  * print_listint - Print elements of a singly linked list.
  * @h: Pointer to a list.
+ *
  * Return: Integer.
  **/
-
 size_t print_listint(const listint_t *h)
 {
-	size_t counter = 0;
+size_t counter = 0;
 
-	if (!h)
-		return (0);
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		counter++;
-	}
-	return (counter);
+if (!h)
+return (0);
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+counter++;
+}
+return (counter);
 }
